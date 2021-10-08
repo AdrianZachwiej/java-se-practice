ba409c2f178fe50943ce05cec36c8e0b751d2b79
AdrianZachwiej <90029778+AdrianZachwiej@users.noreply.github.com>
2021-10-08 15:11:41 +0200

    Add files via upload

---
+++ Exercises/EX52Sets/DwarfPlanet.java
@@ -0,0 +1,7 @@
+package Exercises.EX52Sets;
+
+public class DwarfPlanet extends HeavenlyBody {
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
